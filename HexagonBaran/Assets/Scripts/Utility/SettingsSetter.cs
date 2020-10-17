7eea25b71cc68d1ce715fca8ce41fe7cf231c936
Ismet Baran Surucu <baransrc@gmail.com>
2020-10-17 03:25:04 +0300

    Project Settings and Cleanup.

---
+++ HexagonBaran/Assets/Scripts/Utility/SettingsSetter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsSetter : MonoBehaviour
+{
+    [SerializeField] private int targetFrameRate;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = targetFrameRate;
+        
+    }
+}
